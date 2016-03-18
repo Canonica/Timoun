b09b80efc9de05309b58d093d3f66b0d8f7686cc
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2016-03-18 18:56:24 +0100

    first version

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public int playerID;
+    public int health;
+    public int stamina;
+    public bool canAttack;
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+        canAttack = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator skillCD(int cdBetween)
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(cdBetween);
+        canAttack = true;
+    }
+}
---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+
+    private static TurnManager instance = null;
+
+    enum gameState
+    {
+        Menu,
+        Pause,
+        GameOver,
+        Playing
+    };
+
+    gameState currentGamestate;
+
+    public static TurnManager GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
