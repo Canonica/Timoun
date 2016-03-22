eddb76dae9df3e0dbf407a0b553b2a1e79b44403
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2016-03-22 16:57:18 +0100

    no message

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,52 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour {
     public int playerID;
+
+
+    public int healthMax;
     public int health;
+    public Image healthImage;
+
     public int stamina;
     public bool canAttack;
     public Vector3 position;
+    public Monster target;
 	// Use this for initialization
 	void Start () {
         canAttack = true;
+        healthMax = 40;
+        stamina = 40;
+        health = healthMax;
+        healthImage = GameObject.Find("HealthFull" + (playerID + 1)).GetComponent<Image>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        healthImage.fillAmount = (float)((float)health / (float)healthMax);
+    }
 
     IEnumerator skillCD(int cdBetween)
     {
         canAttack = false;
         yield return new WaitForSeconds(cdBetween);
         canAttack = true;
+    }
+
+    public void getDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Dead();
+        }
+    }
+
+    void Dead()
+    {
+        Debug.Log(playerID + " is dead");
     }
 }
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,20 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TurnManager : MonoBehaviour {
 
     private static TurnManager instance = null;
 
-    enum gameState
-    {
-        Menu,
-        Pause,
-        GameOver,
-        Playing
-    };
+    public Image timer;
+    public float maxTimeTurn;
+    public float currentTimeTurn;
+    public bool isPlaying;
 
-    gameState currentGamestate;
-
+    public int whoPlays;
     public static TurnManager GetInstance()
     {
         return instance;
@@ -24,5 +21,56 @@ public class TurnManager : MonoBehaviour {
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
+        maxTimeTurn = 10;
+        currentTimeTurn = maxTimeTurn;
+        isPlaying = false;
+        whoPlays = 0;
+    }
+
+    void Update()
+    {
+        Debug.Log(whoPlays);
+        currentTimeTurn -= Time.deltaTime;
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            StartCoroutine(WaitForCdfloat(maxTimeTurn));
+        }
+        if(currentTimeTurn <= 0)
+        {
+            changeTurn();
+        }
+    }
+
+    void changeTurn()
+    {
+        currentTimeTurn = maxTimeTurn;
+        timer.fillAmount = 1;
+        StartCoroutine(WaitForCdfloat(maxTimeTurn));
+        if(whoPlays == 0)
+        {
+            whoPlays = 1;
+        }else if (whoPlays == 1)
+        {
+            whoPlays = 0;
+        }
+    }
+
+    public IEnumerator WaitForCdfloat(float cdTimer)
+    {
+            timer.fillAmount = 1;
+            yield return StartCoroutine(fillIcon(timer, cdTimer));
+    }
+
+    public IEnumerator fillIcon(Image icon, float cdTimer)
+    {
+        float timer = maxTimeTurn;
+        while (timer <= cdTimer)
+        {
+            icon.fillAmount = timer / cdTimer;
+            timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        timer = 0;
     }
 }
